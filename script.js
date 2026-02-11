535285454e24ab9a8f8d7251eec7f3194b7ee625
StrataEdition <strata.edition@gmail.com>
2026-02-11 11:37:09 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ const RELEASE_DATA = {
     "STR-005": {
         img: "assets/ParallelRooms1024x1024.jpg",
         title: "PARALLEL ROOMS",
-        quotes: [''],
+        quotes: [],
         bandcamp: `<iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/album=1255637387/size=large/bgcol=ffffff/linkcol=333333/tracklist=false/artwork=small/transparent=true/" seamless><a href="https://strataedition.bandcamp.com/album/parallel-rooms">Parallel Rooms by Pietro Bardini</a></iframe>`,
         youtube: []
     },
